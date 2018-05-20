28981908023032f50b5aab9ca9780804a796fb8d
Mahesh291 <38691516+Mahesh291@users.noreply.github.com>
2018-05-20 17:24:11 +0530

    Simple chatbot in android
    This chatbot is made in hackathon organized by IIITDMJ by me. It uses IBM Watson API which accepts request by user and provide suitable response.

---
+++ Ryuk/app/src/main/java/com/example/harshitgupta/naam/Model.java
@@ -0,0 +1,44 @@
+package com.example.harshitgupta.naam;
+
+/**
+ * Created by Harshit Gupta on 3/9/2018.
+ */
+
+public class Model {
+    private String user;
+    private String id;
+    private String message1;
+    private String message2;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage1() {
+        return message1;
+    }
+
+    public void setMessage1(String message1) {
+        this.message1 = message1;
+    }
+
+    public String getMessage2() {
+        return message2;
+    }
+
+    public void setMessage2(String message2) {
+        this.message2 = message2;
+    }
+}
